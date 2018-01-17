0efeb46ad4b650db40a1d9c947cfc537237fb8b8
Augustinas <ajokubauskass@gmail.com>
2018-01-17 23:57:52 +0000

    Move to iframe approach for one drive

--- src/index.js
+++ src/index.js
@@ -4,37 +4,36 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-function oneDriveChildHandler() {
-  function getFiles(options) {
-    return new Promise((res, rej) => {
-      window.OneDrive.open(
-        Object.assign(
-          {
-            success: files => res([undefined, files]),
-            error: e => res([e, undefined])
-          },
-          options
-        )
-      );
-    });
-  }
+// function oneDriveChildHandler() {
+//   function getFiles(options) {
+//     return new Promise((res, rej) => {
+//       window.OneDrive.open(
+//         Object.assign(
+//           {
+//             success: files => res([undefined, files]),
+//             error: e => res([e, undefined]),
+//             cancel: () => res([])
+//           },
+//           options
+//         )
+//       );
+//     });
+//   }
 
-  async function handleMessages(event) {
-    // To prevent other domains as well as onedrive library messages
-    if (event.origin !== window.origin || !event.data.includes("clientId")) {
-      return;
-    }
-    const data = JSON.parse(event.data);
-    const resp = await getFiles(data);
-    window.opener.postMessage(JSON.stringify(resp), window.origin);
-  }
+//   async function handleMessages(event) {
+//     // To prevent other domains as well as onedrive library messages
+//     if (event.origin !== window.origin || !event.data.includes("clientId")) {
+//       return;
+//     }
+//     const data = JSON.parse(event.data);
+//     const resp = await getFiles(data);
+//     window.opener.postMessage(JSON.stringify(resp), window.origin);
+//   }
 
-  window.addEventListener("message", handleMessages);
-}
+//   window.addEventListener("message", handleMessages);
+// }
 
-if (window.location.pathname === "/onedrive") {
-  oneDriveChildHandler();
-}
+//   oneDriveChildHandler();
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
--- src/onedrive.js
+++ src/onedrive.js
@@ -1,32 +1,29 @@
 export function getFiles(options) {
   return new Promise((res, rej) => {
-    const url = `${window.location.origin}/onedrive`;
     // Had a bug where request was sent twice
     let resolved = false;
-    const middlewareWindow = window.open(
-      url,
-      "Popup",
-      "location,status,scrollbars,resizable,width=1, height=1"
-    );
+    let iFrame;
 
-    function sendOnedriveOptions() {
-      middlewareWindow.postMessage(
-        JSON.stringify(options),
-        window.location.origin
-      );
+    function sendOnedriveOptions(receiver) {
+      receiver.postMessage(JSON.stringify(options), window.location.origin);
     }
 
     function handleMessages(event) {
       const data = JSON.parse(event.data);
       window.removeEventListener("message", handleMessages);
-      middlewareWindow.removeEventListener("load", sendOnedriveOptions);
-      middlewareWindow.close();
+      // TODO close iWindow
+      iFrame.parentNode.removeChild(iFrame);
       if (!resolved) {
         resolved = true;
         res(data);
       }
     }
-    middlewareWindow.addEventListener("load", sendOnedriveOptions);
     window.addEventListener("message", handleMessages);
+
+    iFrame = document.createElement("iframe");
+    iFrame.src = "/onedrive.html";
+    iFrame.style.display = "none";
+    iFrame.onload = () => sendOnedriveOptions(iFrame.contentWindow);
+    document.body.appendChild(iFrame);
   });
 }
